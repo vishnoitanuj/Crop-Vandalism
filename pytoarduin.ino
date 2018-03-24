51f778817224c7f7d5c8899088c99e17df4250a3
TANUJ VISHNOI <vishnoitanuj@outlook.com>
2018-03-24 17:15:45 +0530

    Add files via upload

---
+++ pytoarduin.ino
@@ -0,0 +1,20 @@
+#define LED 8
+
+// Using http://slides.justen.eng.br/python-e-arduino as refference
+
+void setup() {
+    pinMode(LED, OUTPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    if (Serial.available()) {
+        char serialListener = Serial.read();
+        if (serialListener == 'H') {
+            digitalWrite(LED, HIGH);
+        }
+        else if (serialListener == 'L') {
+            digitalWrite(LED, LOW);
+        }
+    }
+}
